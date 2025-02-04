a0076c5a58a40a8df9d7452675c314b34f06acdf
AlinaHan1 <alinakoshenko@gmail.com>
2025-02-05 01:15:15 +0300

    Add files via upload

--- project_app/app/routers/task.py
+++ project_app/app/routers/task.py
@@ -1,28 +1,70 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session  # Сессия БД
+from app.backend.db_depends import get_db  # Функция подключения к БД
+from typing import Annotated  # Аннотации, Модели БД и Pydantic.
+from app.models import Task, User
+from app.schemas.task import CreateTask, UpdateTask
+from app.schemas.user import CreateUser
+from sqlalchemy import insert, select, update, delete  # Функции работы с записями.
+
+from slugify import slugify
 
 router = APIRouter(prefix='/task', tags=['task'])
+DbSession = Annotated[Session, Depends(get_db)]
 
 
 @router.get('/')
-async def all_tasks():
-    pass
+async def all_tasks(db: DbSession):
+    """Получить список всех задач"""
+    return db.scalars(select(Task)).all()
 
 
 @router.get('/task_id')
-async def task_by_id():
-    pass
+async def task_by_id(db: DbSession, task_id: int):
+    """Задача по идентификатору"""
+    task = db.scalar(select(Task).where(Task.id == task_id))
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Task was not found')
+    return task
 
 
 @router.post('/create')
-async def create_task():
-    pass
+async def create_task(db: DbSession, task_create: CreateTask, user_id: int):
+    """Создать новую задачу"""
+    user = db.scalar(select(User).where(User.id == user_id))
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    db.execute(insert(Task).values(
+        title=task_create.title,
+        content=task_create.content,
+        user_id=user_id,
+        priority=task_create.priority,
+        slug=slugify(task_create.title)))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
 
 
 @router.put('/update_task')
-async def update_task():
-    pass
+async def update_task(db: DbSession, task_id: int, task_up: UpdateTask):
+    """Обновить задачу"""
+    task = db.scalar(select(Task).where(Task.id == task_id))
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Task was not found')
+    db.execute(update(Task).where(Task.id == task_id).values(
+        title=task_up.title,
+        content=task_up.content,
+        priority=task_up.priority,
+        slug=slugify(task_up.title)))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'Task has been deleted'}
 
 
 @router.delete('/delete')
-async def delete_task():
-    pass
+async def delete_task(db: DbSession, task_id: int):
+    """Удалить задачу"""
+    task = db.scalar(select(Task).where(Task.id == task_id))
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Task was not found')
+    db.execute(delete(Task).where(Task.id == task_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'Task has been deleted'}
--- project_app/app/routers/user.py
+++ project_app/app/routers/user.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session  # Сессия БД
 from app.backend.db_depends import get_db  # Функция подключения к БД
 from typing import Annotated  # Аннотации, Модели БД и Pydantic.
-from app.models import User
+from app.models import User, Task
 from app.schemas.user import CreateUser, UpdateUser
+from app.schemas.task import CreateTask
 from sqlalchemy import insert, select, update, delete  # Функции работы с записями.
 
 from slugify import slugify
@@ -27,6 +28,14 @@ async def user_by_id(db: DbSession, user_id: int):
     return user
 
 
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(db: DbSession, task_create: CreateTask, user_id: int):
+    tasks = db.scalars(select(Task).where(User.id == user_id)).all()
+    if tasks is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    return tasks
+
+
 @router.post('/create')
 async def create_user(user: CreateUser, db: DbSession) -> dict:
     """Создать нового пользователя"""
@@ -47,11 +56,10 @@ async def update_user(db: DbSession, user_id: int, user_up: UpdateUser):
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
     db.execute(update(User).where(User.id == user_id).values(
-        username=user.username,
         firstname=user_up.firstname,
         lastname=user_up.lastname,
         age=user_up.age,
-        slug=slugify(user_up.username)))
+        slug=slugify(user_up.firstname)))
     db.commit()
     return {'status_code': status.HTTP_200_OK, 'transaction': 'User has been deleted'}
 
@@ -60,8 +68,11 @@ async def update_user(db: DbSession, user_id: int, user_up: UpdateUser):
 async def delete_user(db: DbSession, user_id: int):
     """Удалить пользователя"""
     user = db.scalar(select(User).where(User.id == user_id))
+    tasks_delete = db.scalars(select(Task).where(Task.user_id == user_id))
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
     db.execute(delete(User).where(User.id == user_id))
+    if tasks_delete:
+        db.execute(delete(Task).where(Task.user_id == user_id))
     db.commit()
     return {'status_code': status.HTTP_200_OK, 'transaction': 'User has been deleted'}
